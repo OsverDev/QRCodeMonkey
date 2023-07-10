0398fba363115c139c1833a0114da92dbc74e1e4
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 10:19:53 -0600

    use full http url

--- pullQRcode.php
+++ pullQRcode.php
@@ -3,30 +3,12 @@
 
 
 
-$reqURL = $_SERVER['DOCUMENT_ROOT'].'/qrcode.php';
+$reqURL = 'https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=https://www.echowho.com/pair/sdfsg2h121wq';
 $temp = tempnam(sys_get_temp_dir(), 'TMP_');
-
-
-$getdata = http_build_query(
-    array(
-     'd'=>'Test4',
-     )
-    );
-    
-    $opts = array('http' =>
-     array(
-        'method'  => 'GET',
-        'content' => $getdata
-    )
-    );
-    
-    $context  = stream_context_create($opts);
-    
-    $result = file_get_contents($reqURL, true, $context);
 
 file_put_contents($temp, file_get_contents("$reqURL"));
 
-//header('Content-type: image/png');
+header('Content-type: image/png');
 echo file_get_contents($temp);
 //echo $reqURL;
 fclose($temp); // this removes the file

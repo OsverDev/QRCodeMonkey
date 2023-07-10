e29d80ce9212fab0533ff5b969388a896ce4a1f0
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:25:27 -0600

    Test1

--- pullQRcode.php
+++ pullQRcode.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$content = file_get_contents('https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=https://www.echowho.com/pair/sdfsg2h121wq');
+$content = file_get_contents('https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=Test1');
 file_put_contents('../test/img.png', $content);
 
 header('Content-type: image/png');

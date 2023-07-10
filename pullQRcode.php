4196962e7c24b3afd3bb77f58f5647235e633669
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:05:51 -0600

    try file get contents

--- pullQRcode.php
+++ pullQRcode.php
@@ -1,5 +1,9 @@
 <?php
 
-echo copy("qrcode.php","temp/image.php");
+$url = 'qrcode.php?s=qr-h&d=https://www.echowho.com/pair/sdfsg2h121wq';
+//$img = '/my/folder/flower.gif';
+//file_put_contents($img, file_get_contents($url));
+
+echo file_get_contents($url);
 
 ?>

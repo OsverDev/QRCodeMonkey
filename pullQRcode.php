524b213b6170b45691a4125ff86abf692b2c9b5d
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:23:20 -0600

    change header type

--- pullQRcode.php
+++ pullQRcode.php
@@ -4,6 +4,7 @@
 $content = file_get_contents('https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=https://www.echowho.com/pair/sdfsg2h121wq');
 file_put_contents('../test/img.png', $content);
 
-file_get_contents('../test/img.png');
+header('Content-type: image/png');
+echo file_get_contents('../test/img.png');
 
 ?>

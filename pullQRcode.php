288c0fa25ba0c6bcefa9bdee4d735d3854644ebb
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:34:11 -0600

    get right image

--- pullQRcode.php
+++ pullQRcode.php
@@ -5,7 +5,7 @@ $content = file_get_contents('https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=ht
 file_put_contents('img.png', $content);
 
 header('Content-type: image/png');
-echo file_get_contents('../test/img.png');
+echo file_get_contents('img.png');
 
 
 ?>

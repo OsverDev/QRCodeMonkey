90dd983337ebb3c970f7a449e0873a3e96013e1c
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:11:46 -0600

    just use path name

--- pullQRcode.php
+++ pullQRcode.php
@@ -1,7 +1,7 @@
 <?php
 
 $url = 'https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=https://www.echowho.com/pair/sdfsg2h121wq';
-$img = '/temp/img.png';
+$img = '/temp/';
 file_put_contents($img, file_get_contents($url));
 
 echo file_get_contents($img);

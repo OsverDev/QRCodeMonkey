3f1726a65b30937b2007e761168117e223e307d1
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 10:07:32 -0600

    Dele unessary code

--- pullQRcode.php
+++ pullQRcode.php
@@ -1,38 +1,13 @@
 <?php
 
 
-$reqURL = $_SERVER['DOCUMENT_ROOT'].'/xqrcode.php?';
+$reqURL = $_SERVER['DOCUMENT_ROOT'].'/qrcode.php?s=qr-h&d=https://www.echowho.com/pair/sdfsg2h121wq';
 $temp = tempnam(sys_get_temp_dir(), 'TMP_');
 
-
-// Create a stream
-$opts = array(
-    'http'=>array(
-      'method'=>"GET",
-      'header'=>"Accept-language: en\r\n" .
-                "Cookie: foo=bar\r\n",
-        ""
-    )
-  );
-  
-  $context = stream_context_create($opts);
-  
-  // Open the file using the HTTP headers set above
-  $file = file_get_contents('http://www.example.com/', false, $context);
-
 file_put_contents($temp, file_get_contents("$reqURL"));
-//file_put_contents($temp, $content);
+
 header('Content-type: image/png');
 echo file_get_contents($temp);
 fclose($temp); // this removes the file
-
-
-
-// $dir    = $_SERVER['DOCUMENT_ROOT'];
-// $files1 = scandir($dir);
-// $files2 = scandir($dir, SCANDIR_SORT_DESCENDING);
-
-// print_r($files1);
-// print_r($files2);
 
 ?>

f83dbd357610c7ecaa6a119f9928807d2425d845
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:21:39 -0600

    get image we just saved

--- pullQRcode.php
+++ pullQRcode.php
@@ -4,4 +4,6 @@
 $content = file_get_contents('https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=https://www.echowho.com/pair/sdfsg2h121wq');
 file_put_contents('../test/img.png', $content);
 
+file_get_contents('../test/img.png');
+
 ?>

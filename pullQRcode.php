6329ab52b4810e24076b1de864408067ce53c65d
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:41:55 -0600

    new tempfile name

--- pullQRcode.php
+++ pullQRcode.php
@@ -1,10 +1,12 @@
 <?php
 
 
-$content = file_get_contents('https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=Test3');
-echo $temp = tmpfile();
-file_put_contents($temp, $content);
-//header('Content-type: image/png');
+$reqURL = 'https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=Test3';
+$temp = tempnam(sys_get_temp_dir(), 'TMP_');
+
+file_put_contents($temp, file_get_contents("$path/$filename"));
+//file_put_contents($temp, $content);
+header('Content-type: image/png');
 echo file_get_contents($temp);
 fclose($temp); // this removes the file
 

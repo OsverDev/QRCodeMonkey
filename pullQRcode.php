ac5f5e35d025763bd86e02ea42b0faaf96af8b1b
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:42:35 -0600

    changed variables

--- pullQRcode.php
+++ pullQRcode.php
@@ -4,7 +4,7 @@
 $reqURL = 'https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=Test3';
 $temp = tempnam(sys_get_temp_dir(), 'TMP_');
 
-file_put_contents($temp, file_get_contents("$path/$filename"));
+file_put_contents($temp, file_get_contents("$reqURL"));
 //file_put_contents($temp, $content);
 header('Content-type: image/png');
 echo file_get_contents($temp);

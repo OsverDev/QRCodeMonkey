185b5bd5bdf66c4a9a4b3071c697f34e4b7eaedc
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:39:01 -0600

    try using tempfile

--- pullQRcode.php
+++ pullQRcode.php
@@ -2,10 +2,10 @@
 
 
 $content = file_get_contents('https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=Test3');
-file_put_contents('img.png', $content);
-
+$temp = tmpfile();
+file_put_contents($temp, $content);
 header('Content-type: image/png');
-echo file_get_contents('img.png');
-
+echo file_get_contents($temp);
+fclose($temp); // this removes the file
 
 ?>

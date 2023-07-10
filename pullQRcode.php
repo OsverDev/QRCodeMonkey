962fe7f7b7fabc653a6f7b5bee8d6bc3778eed9a
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:30:35 -0600

    dont use temp folder

--- pullQRcode.php
+++ pullQRcode.php
@@ -2,7 +2,7 @@
 
 
 $content = file_get_contents('../qrcode.php?s=qr-h&d=Test2');
-file_put_contents('../test/img.png', $content);
+file_put_contents('img.png', $content);
 
 header('Content-type: image/png');
 echo file_get_contents('../test/img.png');

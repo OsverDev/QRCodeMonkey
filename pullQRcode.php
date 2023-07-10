93f92fafe206678026bbb7faac17e98c926cc7d6
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:16:15 -0600

    Allow Any IP

--- pullQRcode.php
+++ pullQRcode.php
@@ -1,14 +1,7 @@
 <?php
 
 
-$ch = curl_init('https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=https://www.echowho.com/pair/sdfsg2h121wq');
-$fp = fopen('/temp/img.png', 'wb');
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_exec($ch);
-curl_close($ch);
-fclose($fp);
-
-echo file_get_contents('/temp/img.png');
+$content = file_get_contents('https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=https://www.echowho.com/pair/sdfsg2h121wq');
+file_put_contents('/my/folder/flower.jpg', $content);
 
 ?>

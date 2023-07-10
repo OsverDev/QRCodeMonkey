1074772e451e40829522239543407373b4d192c9
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:09:33 -0600

    Download, Save. Show

--- pullQRcode.php
+++ pullQRcode.php
@@ -1,9 +1,9 @@
 <?php
 
 $url = 'https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=https://www.echowho.com/pair/sdfsg2h121wq';
-//$img = '/my/folder/flower.gif';
-//file_put_contents($img, file_get_contents($url));
+$img = '/my/folder/flower.gif';
+file_put_contents($img, file_get_contents($url));
 
-echo file_get_contents($url);
+echo file_get_contents($img);
 
 ?>

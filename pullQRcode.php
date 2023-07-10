754479c3bef92e011b2dca8e2ffde92dbbbeea5b
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-07-10 09:52:50 -0600

    remove all parametsrs

--- pullQRcode.php
+++ pullQRcode.php
@@ -1,8 +1,24 @@
 <?php
 
 
-$reqURL = 'https://qrcodes.myjson.site/qrcode.php?s=qr-h&d=Test3';
+$reqURL = 'qrcode.php?';
 $temp = tempnam(sys_get_temp_dir(), 'TMP_');
+
+
+// Create a stream
+$opts = array(
+    'http'=>array(
+      'method'=>"GET",
+      'header'=>"Accept-language: en\r\n" .
+                "Cookie: foo=bar\r\n",
+        ""
+    )
+  );
+  
+  $context = stream_context_create($opts);
+  
+  // Open the file using the HTTP headers set above
+  $file = file_get_contents('http://www.example.com/', false, $context);
 
 file_put_contents($temp, file_get_contents("$reqURL"));
 //file_put_contents($temp, $content);
